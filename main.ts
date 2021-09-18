d8238b6581424ea38d0c9476d569cc8cbe5f882e
shanevit <38755375+shanevit@users.noreply.github.com>
2021-09-18 09:49:16 +0200

    Aktualizovat main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,70 @@
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(randint(1, 6))
 })
+input.onButtonPressed(Button.AB, function () {
+    hod = randint(1, 6)
+    if (hod == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (hod == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # . # .
+            . . . . .
+            . . . . .
+            `)
+    } else if (hod == 3) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (hod == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (hod == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(randint(1, 6) + randint(1, 6))
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    hod = randint(1, 6)
+    for (let index = 0; index < hod; index++) {
+        music.playTone(262, music.beat(BeatFraction.Quarter))
+        music.rest(music.beat(BeatFraction.Quarter))
+    }
+})
+let hod = 0
+music.setBuiltInSpeakerEnabled(true)
 basic.forever(function () {
 	
 })
